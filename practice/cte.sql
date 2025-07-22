4bd9690b1d50d8c248f1f24b291d5130bcb2a1fa
OkokoJnr <96591819+OkokoJnr@users.noreply.github.com>
2025-07-22 16:04:50 +0100

    practice task: rank customers

--- practice/cte.sql
+++ practice/cte.sql
@@ -17,7 +17,14 @@ WITH cte_total_sales AS (
     GROUP BY customerid
     )
 
-
+--Rank customers base on total sales per customers
+,cte_rank_customers AS (
+    SELECT
+        customerid,
+        total_sales,
+        RANK() OVER (ORDER BY total_sales DESC) AS customers_rank
+    FROM cte_total_sales
+)
 --Main Query
 SELECT 
     c.customerid,
@@ -31,4 +38,5 @@ LEFT JOIN cte_total_sales cte_ts
 ON c.customerid = cte_ts.customerid
 LEFT JOIN cte_last_order_date cte_lod
 ON c.customerid = cte_lod.customerid
-
+LEFT JOIN cte_rank_customers cte_rc
+ON c.customerid = cte_rc.customerid
